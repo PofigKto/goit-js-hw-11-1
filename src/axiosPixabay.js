f6cea5c0e5c7f3818575228c1cdc4eb2396e6c3f
Protopoop Aleksandr <alexandr2723@gmail.com>
2022-10-28 17:51:38 +0300

    Update axiosPixabay.js

--- src/axiosPixabay.js
+++ src/axiosPixabay.js
@@ -1,9 +1,39 @@
 // === именованный экспорт функции которая делает запрос на бекенд и возвращает промис
 // import axios from 'axios';
 const axios = require('axios');
-const API_KEY = '30745255-b909a2241bf9ab8444da42817';
+const API_KEY = '25154920-bc2b97b916e9c15e1ff6fb5dd';
 const BASE_URL = 'https://pixabay.com/api';
+// ==============запрос через фетч================
+// export default class ImageApiService {
+//   constructor() {
+//     this.searchQuery = '';
+//     this.page = 1;
+//   }
+//   fetchFotos() {
+//     console.log(this);
 
+//     const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+//     return fetch(url)
+//       .then(response => response.json())
+//       .then(data => {
+//         this.icrementPage();
+//         console.log(data);
+//         return data;
+//       });
+//   }
+//   icrementPage() {
+//     this.page += 1;
+//   }
+//   resetPage() {
+//     this.page = 1;
+//   }
+//   get query() {
+//     return this.searchQuery;
+//   }
+//   set query(newQuery) {
+//     this.query - newQuery;
+//   }
+// };
 
 export default class ImageApiService {
   constructor() {
@@ -15,7 +45,7 @@ export default class ImageApiService {
     // console.log(this);
     const params = new URLSearchParams({
       q: this.searchQuery,
-      key: '30745255-b909a2241bf9ab8444da42817',
+      key: '25154920-bc2b97b916e9c15e1ff6fb5dd',
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
